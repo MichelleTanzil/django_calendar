40ae6a0f8b55452df16bb2c25c8b0439c4c2aca4
Michelle Tanzil <mtanzil01@gmail.com>
2019-11-14 14:31:36 -0800

    first commit

---
+++ apps/cal/admin.py
@@ -0,0 +1,5 @@
+from django.contrib import admin
+from apps.cal.models import Event
+
+# Register your models here.
+admin.site.register(Event)
---
+++ apps/cal/urls.py
@@ -0,0 +1,8 @@
+from django.conf.urls import url
+from . import views
+
+app_name = 'cal'
+urlpatterns = [
+    url(r'^$', views.index, name='index'),
+    url(r'^calendar$', views.CalendarView.as_view(), name='calendar'),
+]
